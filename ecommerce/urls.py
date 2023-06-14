41b3ffc26eae335ca8e51049953c68d56394d6bf
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2023-06-14 20:29:06 +0400

    Fix routing url patterns

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,10 +22,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Include djoser urls in main url patterns
-    path('api/v1', include('djoser.urls')),
-    path('api/v1', include('djoser.urls.authtoken')),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken')),
     # Include url patterns of product app
-    path('api/v1', include('product.urls')),
+    path('api/v1/', include('product.urls')),
 ]
 
 if settings.DEBUG:
